e7790bfbb7a0f73e1cf0d004d8c7a7ee71fe6e14
yoon <missingtttt@gmail.com>
2024-10-07 20:00:39 +0900

    20241007 - 클래스 시작(클래스, 읽기, 쓰기)

---
+++ src/class1/ClassStart1.java
@@ -0,0 +1,18 @@
+package class1;
+
+public class ClassStart1 {
+
+    public static void main(String[] args) {
+        String studentName1 = "학생1";
+        int studentAge1 = 15;
+        int studentGrade1 = 90;
+
+        String studentName2 = "학생2";
+        int studentAge2 = 16;
+        int studentGrade2 = 80;
+
+        System.out.println("이름: " + studentName1 + " 나이: " + studentAge1 + " 성적: " + studentGrade1);
+        System.out.println("이름: " + studentName2 + " 나이: " + studentAge2 + " 성적: " + studentGrade2);
+
+    }
+}
