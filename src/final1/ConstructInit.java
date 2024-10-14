f7b5a82f39fafc42f4e5742986bd82757f7ef86a
Yoonseongwon <missingtttt@gmail.com>
2024-10-15 05:10:02 +0900

    20241015 - final 변수, static  in notebook

---
+++ src/final1/ConstructInit.java
@@ -0,0 +1,10 @@
+package final1;
+
+public class ConstructInit {
+
+    final int value;
+
+    public ConstructInit(int value) {
+        this.value = value;  // 생성자를 통해 final 변수를 초기화 할 수 있음 .
+    }
+}
---
+++ src/final1/FieldInit.java
@@ -0,0 +1,13 @@
+package final1;
+
+public class FieldInit {
+
+    static final int CONST_VALUE = 10;
+    final int value = 10;
+
+    /*
+    public FieldInit(int value) {
+        //this.value = value; //final 지역 변수에 이미 초기화가 된 경우에는 생성자를 통해서도 다시 초기화 안됨.
+    }
+    */
+}
