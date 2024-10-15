fa19d334176245cb04667012334bf424de4d150c
Yoonseongwon <missingtttt@gmail.com>
2024-10-16 05:13:13 +0900

    20241016 - 상속

---
+++ src/extend1/access/parent/Parent.java
@@ -0,0 +1,17 @@
+package extend1.access.parent;
+
+public class Parent {
+
+    public int publicValue;
+    protected int protectedValue;
+    int defaultValue;
+    private int privateValue;
+
+    public void publicMethod() {
+        System.out.println("Parent.publicMethod");
+    }
+
+    public void protectedMethod() {
+        System.out.println("Parent");
+    }
+}
